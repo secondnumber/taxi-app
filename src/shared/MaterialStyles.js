0199e0ac9482c48a59640a5d552bbcc7ec025c0d
Yulya Vorontsova <secondnumber01@gmail.com>
2019-10-29 10:14:22 +0300

    refactor: deleted excess import

--- src/shared/MaterialStyles.js
+++ src/shared/MaterialStyles.js
@@ -1,5 +1,3 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import "../assets/css/Form.css";
